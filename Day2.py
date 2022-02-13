0a8900b17b4aac5111e5cce1ec3277776d38c641
AdalbertoParedes <96451831+AdalbertoParedes@users.noreply.github.com>
2022-02-13 13:26:17 -0600

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip calculator!")
+bill_total = input ("What was the total bill? $")
+tip_total = input("How much tip would you like to give? 10, 12, or 15? ")
+people_total= input("How many people to split the bill? ")
+
+floatbill = float(bill_total)
+floattip = float(tip_total)
+intpeople = int(people_total)
+
+topay = (   (floatbill * (1 + (floattip / 100))) / intpeople      )
+
+roundtopay = round(topay,2)
+print(f"Each person should pay: ${roundtopay}")
