165801823c6e1e32d942a31eb59b51c7b7b92045
AdalbertoParedes <96451831+AdalbertoParedes@users.noreply.github.com>
2022-02-13 13:37:42 -0600

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -9,5 +9,9 @@ intpeople = int(people_total)
 
 topay = (   (floatbill * (1 + (floattip / 100))) / intpeople      )
 
+
 roundtopay = round(topay,2)
+
+#in case the total only have one decimal, we use this format to place the "missing" cero
+roundtopay = "{:.2f}".format(topay)
 print(f"Each person should pay: ${roundtopay}")
